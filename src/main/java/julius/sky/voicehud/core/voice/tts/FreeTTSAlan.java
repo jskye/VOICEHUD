87fb66bc881c2ac5b8d6ef69589686978c2ce543
Julius Sky <julius.skye@gmail.com>
2015-10-15 18:29:16 +1100

    basic freetts
    basic freetts voice working.
    call with TTS.doSpeak().

---
+++ src/main/java/julius/sky/voicehud/core/voice/tts/FreeTTSAlan.java
@@ -0,0 +1,21 @@
+//package julius.sky.voicehud.core.voice.tts;
+//import com.sun.speech.freetts.VoiceManager;
+//import com.sun.speech.freetts.Voice;
+//
+//public class FreeTTSAlan {
+//
+//
+//	    public static void main(String[] args) {
+//	        VoiceManager vm = VoiceManager.getInstance();
+//
+//	        Voice v = vm.getVoice("alan");
+//	        String text = "09:20";
+//	        String kata[] = text.split(" ");
+//	        v.allocate();
+//	        for (int i = 0; i < kata.length; i++) {
+//	            v.speak(kata[i]);
+//
+//	        }
+//	        v.deallocate();
+//	    }
+//	}
