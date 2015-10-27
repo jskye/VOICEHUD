465e1e507afec8dd2c6e8222bfb6facd84e216c3
Julius Sky <julius.skye@gmail.com>
2015-10-27 16:40:30 +1100

    messages controller fix
    fix messages controller to speak messages.

--- src/main/java/julius/sky/voicehud/core/hud/MessagesController.java
+++ src/main/java/julius/sky/voicehud/core/hud/MessagesController.java
@@ -1,5 +1,13 @@
 package julius.sky.voicehud.core.hud;
 
+import java.beans.PropertyVetoException;
+
+import javax.speech.AudioException;
+import javax.speech.EngineException;
+import javax.speech.EngineStateError;
+
+import julius.sky.voicehud.core.voice.tts.TTS;
+
 import com.jme3.app.SimpleApplication;
 
 import de.lessvoid.nifty.Nifty;
@@ -15,6 +23,8 @@ public class MessagesController implements ScreenController{
 	private HUDGUIState hudGUI;
 	private Nifty nifty;
 	private Screen screen;
+	private Thread alertThread;
+	private TTS alert;
 
 	/**
 	 * Creates a new controller instance for nifty-gui.
@@ -26,6 +36,25 @@ public class MessagesController implements ScreenController{
 		this.hudGUI = hudGUI;
 		this.nifty = hudGUI.getNifty();
 		this.screen = nifty.getCurrentScreen();
+		
+		try {
+			this.alert = new TTS();
+		} catch (EngineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AudioException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (EngineStateError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		this.alertThread = null;
+
 	}
 
 	public void bind(Nifty arg0, Screen arg1) {
@@ -42,7 +71,22 @@ public class MessagesController implements ScreenController{
 		// TODO Auto-generated method stub
 		setTextToElement("nameLabel1", "Mum");
 		setTextToElement("messageLabel1", "Hello son, how are you today?");
+		this.alert.setTextToSpeak("Mum says: Hello song, how are you today?");
+	    this.alertThread = new Thread(this.alert);
+		speakMessage();
+		try {
+			Thread.sleep(3);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+//		disappear();
 	}
+	
+	public void speakMessage(){
+		alertThread.start();
+}
 	
 	/**
 	 * Sets given text to the given element (e.g. label).
